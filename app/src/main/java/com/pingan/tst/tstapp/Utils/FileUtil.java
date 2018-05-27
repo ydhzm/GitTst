1bf800867bbcdc162ac50de3488fe9b9ecf1a25e
ydh <yangdonghua229>
2018-05-27 12:45:29 +0800

    FileUtil.java add Log

--- app/src/main/java/com/pingan/tst/tstapp/Utils/FileUtil.java
+++ app/src/main/java/com/pingan/tst/tstapp/Utils/FileUtil.java
@@ -1,7 +1,10 @@
 package com.pingan.tst.tstapp.Utils;
 
+import android.util.Log;
+
 public class FileUtil {
     public static void print(String s){
         System.out.println(s);//
+        Log.i("aa",s);
     }
 }
