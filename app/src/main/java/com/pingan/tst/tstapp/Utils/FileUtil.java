b523dbea5f9e7e732da1c6ccea1c00d19570bcfc
ydh <yangdonghua229>
2018-05-27 14:16:00 +0800

    如果路径里面包含pingan，则返回为空

--- app/src/main/java/com/pingan/tst/tstapp/Utils/FileUtil.java
+++ app/src/main/java/com/pingan/tst/tstapp/Utils/FileUtil.java
@@ -2,9 +2,26 @@ package com.pingan.tst.tstapp.Utils;
 
 import android.util.Log;
 
+import java.io.File;
+
 public class FileUtil {
     public static void print(String s){
         System.out.println(s);//
         Log.i("aa",s);
+    }
+
+    public static File[] listAllFiles(File file){
+        if(file==null || !file.exists() || file.getAbsolutePath().contains("pingan")){
+            return null;
+        }
+        if(file.isFile()){
+            File[] fileArray = new File[1];
+             fileArray[0] = file;
+            return fileArray;
+        }
+        if(file.isDirectory()){
+            return file.listFiles();
+        }
+        return null;
     }
 }
